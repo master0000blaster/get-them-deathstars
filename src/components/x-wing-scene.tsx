1df83ae1af31b2aa2ad3426e66edd29b7e0e7350
Joseph Godwin <godwin.joe@gmail.com>
2022-09-25 13:55:25 -0500

    adjusting urls

--- src/components/x-wing-scene.tsx
+++ src/components/x-wing-scene.tsx
@@ -110,7 +110,7 @@ export default function XWingScene(props: XWingSceneProps) {
 
         createPointerLock(scene);
 
-        SceneLoader.ImportMeshAsync('', '/get-them-deathstars/static/3dmodels/', 'xwing.glb', scene)
+        SceneLoader.ImportMeshAsync('', '/static/3dmodels/', 'xwing.glb', scene)
             .then((result: ISceneLoaderAsyncResult) => {
                 if (cameraRef.current) {
                     xwingMeshRef.current = result.meshes[0];
@@ -118,7 +118,7 @@ export default function XWingScene(props: XWingSceneProps) {
                 }
             });
 
-        SceneLoader.ImportMeshAsync('', '/get-them-deathstars/static/3dmodels/', 'deathstar.glb', scene)
+        SceneLoader.ImportMeshAsync('', '/static/3dmodels/', 'deathstar.glb', scene)
             .then((result: ISceneLoaderAsyncResult) => {
                 setupDeathStars(result.meshes[0]);
             });
@@ -184,7 +184,7 @@ export default function XWingScene(props: XWingSceneProps) {
                         intensity={0.7}
                         direction={new Vector3(0.5, 1, 0)}
                     />
-                    <Skybox size={10000} rootUrl="/get-them-deathstars/static/images/space-skybox/" name={'skybox'} />
+                    <Skybox size={10000} rootUrl="/static/images/space-skybox/" name={'skybox'} />
                 </Scene>
             </Engine>
         </div>
