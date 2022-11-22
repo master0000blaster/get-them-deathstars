47bb5db5c95c6358aaed517728d66404f0caf38f
Joseph Godwin <godwin.joe@gmail.com>
2022-11-21 18:15:36 -0600

    enable full dialog

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -111,8 +111,8 @@ export default function Game() {
 
         GameManager.isPaused = true;
         AssetManager.pewSound = new Sound('pew', '/static/sounds/PEW.mp3', scene, null, { loop: false, autoplay: false });
-        AssetManager.introAudio = new Sound('intro', '/static/sounds/PEW.mp3', scene, null, { loop: false, autoplay: true });
-        AssetManager.outroAudio = new Sound('outro', '/static/sounds/PEW.mp3', scene, null, { loop: false, autoplay: false });
+        AssetManager.introAudio = new Sound('intro', '/static/sounds/intro.mp3', scene, null, { loop: false, autoplay: true });
+        AssetManager.outroAudio = new Sound('outro', '/static/sounds/outro.mp3', scene, null, { loop: false, autoplay: false });
         AssetManager.explosionSound = new Sound('explosion', '/static/sounds/explosion.mp3', scene, null, { loop: false, autoplay: false });
         AssetManager.introAudio.onended = introEnded;
         GameManager.outroAudioComlpete = outroEnded;
